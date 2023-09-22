755d76f233d50330c37bd2aa95c3164cf7e327a9
Rizky Bagaskara <78846121+rzkybagaskara@users.noreply.github.com>
2023-09-22 22:45:47 +0700

    Time: 917 ms (66.31%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,4 +3,5 @@ SELECT w1.id
 FROM Weather w1, Weather w2
 WHERE w1.temperature > w2.temperature AND DATEDIFF(w1.recordDate, w2.recordDate) = 1
 
-#DATEDIFF compare the previous day where the difference is one day 
+# Inner JOIN to compare the temperature values
+# DATEDIFF to compare the previous day where the difference is one day 
