f267289c99eb90b54b42680bac6369f4763858d2
Rizky Bagaskara <78846121+rzkybagaskara@users.noreply.github.com>
2023-09-25 22:08:00 +0700

    Time: 903 ms (97.68%), Space: 0B (100.00%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+# Use UNION
+# If a product is not available in a store, do not include a row with that product_id and store combination in the result table.
+
+SELECT product_id, 'store1' AS store, store1 AS price FROM Products WHERE store1 IS NOT NULL
+UNION
+SELECT product_id, 'store2' AS store, store2 AS price FROM Products WHERE store2 IS NOT NULL
+UNION
+SELECT product_id, 'store3' AS store, store3 AS price FROM Products WHERE store3 IS NOT NULL
+
+# Repeat
